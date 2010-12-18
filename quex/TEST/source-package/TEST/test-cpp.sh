1ebdcfb7267730124acd917ff2a08676af6de518
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2010-12-18 04:51:28 +0000

    Option --source-package Implemented
    git-svn-id: https://svn.code.sf.net/p/quex/code/trunk@2500 db9deaaf-66c7-40b0-811a-ad7aae109365

---
+++ quex/TEST/source-package/TEST/test-cpp.sh
@@ -0,0 +1,57 @@
+#! /usr/bin/env bash
+if [[ $1 == "--hwut-info" ]]; then
+    echo "Source Packager: C++;"
+    echo "CHOICES: plain, iconv, icu, codec, codec-utf8, codec-utf16, post-categorizer, no-string-accumulator, no-include-stack, no-counter;"
+    echo "SAME;"
+    exit
+fi
+
+
+case $1 in
+    plain) 
+        option=''
+        ;;
+    iconv)
+        option='--iconv'
+        ;;
+    icu)
+        option='--icu'
+        ;;
+    codec)
+        option='--codec iso8859_7'
+        ;;
+    codec-utf8)
+        option='--codec utf8'
+        ;;
+    codec-utf16)
+        option='--codec utf16 --bes 2'
+        ;;
+    post-categorizer)
+        option='--icu --post-categorizer'
+        ;;
+    no-string-accumulator)
+        option='--no-string-accumulator'
+        ;;
+    no-include-stack)
+        option='--icu --no-include-stack'
+        ;;
+    no-counter)
+        option='--no-count-lines --no-count-columns'
+        ;;
+esac
+
+if [ -d pkg ]; then
+    rm -rf pkg/*
+else
+    mkdir pkg
+fi
+
+echo "(0) Running Quex (no output is good output)"
+quex -i simple.qx -o EasyLexer --source-package pkg $option
+
+echo "(1) Running g++ (no output is good output)"
+g++  -Ipkg -I. pkg/EasyLexer.cpp -o pkg/EasyLexer.o -c -Wall -W
+
+echo "(2) Double check that output file exists"
+ls    pkg/EasyLexer.o
+
---
+++ quex/core_engine/state_machine/compression/barren_trees.py
@@ -0,0 +1,299 @@
+# (C) 2010 Frank-Rene Schaefer
+"""
+   Barren Tree Compression _____________________________________________________
+   
+   The goal of barren tree compression is to be able to express parts of the
+   state machine directly as nested if-then clauses--without the need of 
+   explicit state transitions (i.e. gotos). For example, a state machine that
+   identifies the keywords 'move' and 'mode' as well as the identifiers matching
+   the regular expression [a-z]+ should be written as
+
+
+      /* Something that identifies 'm' */
+      ...
+
+      /* Barren tree to check for 'ove', 'ode' and identifier. */
+BARREN_TREE_ROOT_0:
+      ++input_p;
+      if( *input_p == 'o' ) { 
+          ++input_p;
+          if( *input_p == 'd' ) { 
+              ++input_p;
+              if( *input_p == 'e' ) { 
+                  ++input_p;
+                  goto TERMINAL_MODE;
+              }
+          }
+          else if( *input_p == 'v' ) { 
+              ++input_p;
+              if( *input_p == 'e' ) { 
+                  ++input_p;
+                  goto TERMINAL_MOVE;
+              }
+          }
+      }
+      if( *input_p in [a-z] ) { 
+          goto CHECK_IDENTIFIER;
+      }
+      if( *input_p == BufferLimitCode ) { 
+          reload(...);
+          goto __InitialState;
+      }
+      else { 
+          goto TERMINAL_IDENTIFIER;
+      }
+
+CHECK_IDENTIFIER:
+      ++input_p;
+      if( *input_p >= 'a' && *input_p <= 'z' ) { 
+          goto TERMINAL_IDENTIFIER;
+      }
+      goto TERMINAL_IDENTIFIER;
+      
+   Note, that the detection of 'm' cannot be part of the barren tree, since for
+   if the lexeme does not start with 'm' and is not in [a-z] and not the buffer
+   limit code, then it is a failure. For the barren tree above the state
+   machine transits to success for IDENTIFIER.
+
+
+   Requirements for a state to be part of a barren tree ________________________
+
+       Transition to state requires less then N comparisons
+       (N=2 is may be the best for checking just one range.)
+
+   Result of Analyzis __________________________________________________________
+
+       A barren tree is a set of states organized as a tree. A tree_node
+       contains:
+
+            barren_transition_map[target_index] --> trigger_set 
+
+                This is the list of all transition to subsequent states
+                that can be reached by a 'barren' transition, i.e. a very
+                tiny trigger set.
+
+            fat_transition_map[target_index] --> trigger_set
+
+                Remaining transitions that are not considered to be 
+                'barren'.
+
+            drop_out_info
+
+                Information about 'drop-out' behavior.
+
+      All nodes of the barren tree are accessible via a dictionary, i.e.
+
+            tree_db[state_index] --> tree_node
+
+   _____________________________________________________________________________
+"""
+def get_difference(self, A, B):
+    assert type(A) == dict
+    assert type(B) == dict
+
+    # Compute difference in transition maps
+    #     --> table: [trigger_set, target state in skeleton, target state in candidate]
+    diff_in_A_not_in_B = NumberSet()
+    diff_not_in_A_in_B = NumberSet()
+    diff_in_both       = NumberSet()
+
+    for A_target_index, A_trigger_set in A.items():
+        if B.has_key(target_index) == False:
+            diff_in_A_not_in_B.unite_with(A_trigger_set)
+            continue
+
+        delta_set = B[A_target_index].mutual_exclusive_set(A_trigger_set)
+        if not delta_set.is_empty(): 
+            diff_in_both.unite_with(delta_set)
+            continue
+
+    for B_target_index, B_trigger_set in B.items():
+        if A.has_key(target_index) == False:
+            diff_not_in_A_in_B.unite_with(B_trigger_set)
+            continue
+
+    return diff_in_A_not_in_B, diff_not_in_A_in_B, diff_in_both
+
+class Info:
+    """Note: The path.CharacterPath class a lot in common with the Info.
+
+       The transitions of a state are categorized into 'barren' and 'fat'.
+
+       -- A barren transition is a transition to subsequent state on the 
+          bases of very few comparisons with respect to the input.
+       -- Also, the state to which the barren transition triggers must
+          fit, i.e. its 'fat transitions' and drop-out action must
+          correspond.
+
+       A transition that is not barren is fat.
+    """
+    def __init__(self, State, FatTransitionMap, BarrentTransitionMap):
+        assert isinstance(StartStateIndex, long)
+        assert isinstance(FatTransitionMap, dict)
+
+        self.__state  = State
+
+        # Transitions: target_index --> trigger set that triggers to target
+        #    barren transitions: transitions on small sets to small fitting states.
+        #    fat transitions:    remainder of transitions.
+        self.__barren = BarrenTransitionMap
+        self.__fat    = FatTransitionMap
+
+        # The trigger sets of 'barren' transitions can be occupied by 
+        # arbitrary values, let them be called 'wildcards'.
+        trigger_set = NumberSet()
+        for trigger_set in self.__barren.values():
+            self.__wildcard_set.unite_with(trigger_set)
+
+    def barren(self):
+        return self.__barren
+
+    def fat(self):
+        return self.__fat
+
+    def move_to_fat(self, TargetIndex):
+        trigger_set = self.__fat.get(TargetIndex)
+        if trigger_set == None: self.__fat[TargetIndex] = self.__barren[TargetIndex]
+        else:                   trigger_set.unite_with(self.__barren[TargetIndex])
+        del self.__barren[TargetIndex]
+
+    def plug_wildcards(self, WildcardMap):
+        """The given wildcards become part of the fat transitions."""
+        for target_index, trigger_set in WildcardMap.items():
+            fat_trigger_set = self.__fat.get(target_index)
+            if fat_trigger_set == None: self.__fat[target_index] = trigger_set
+            else:                       fat_trigger_set.unite_with(trigger_set)
+
+            self.__wildcard_set.subtract(trigger_set)
+
+    def match(self, CandidateFatTransitionMap, CandidateBarrenTriggerSet):
+        """-- The candidate **needs** all transitions in the fat transition map
+              in case that no one of its sub-trees is concerned. 
+
+           -- The Tree **can** provide the 'skeleton' transition map and can
+              extend the map characters in the wildcard set.
+
+           RETURNS: NumberSet --> Characters from the wild card that need to be used to 
+                                  achieve the match. Match is good.
+                    empty set --> No wildcard necessary, but match is good.
+                    None      --> No match possible.
+        """
+        # All transitions in the FatTransitionMap must appear in the Skeleton
+        #
+        #     [     5     ][2]     [    3    ][ 1 ]              skeleton
+        #                   *                 *****              wildcard set
+        #
+        #     [     5     ][6]     [    3    ]     [    6     ]  candidate FAIL
+        #     [     5     ][6]     [    3         ]              candidate OK
+        #     [     5     ][1]     [    3         ]              candidate OK
+        #
+        #     -- except for those covered by the wildcards.
+        diff_in_A_not_in_B, \
+        diff_not_in_A_in_B, \
+        diff_in_both      = \
+                get_difference(self.__fat, CandidateFatTransitionMap)
+
+        # (1) what is in self's 'fat' but not in candidate's 'fat transition' 
+        #     can **only** be covered by barren transitions of candidate.
+        if diff_in_A_not_in_B.is_empty() == False:
+            if not CandidateFatTransitionMap.is_superset(diff_in_A_not_in_B): return None
+
+        # (2) If self and candidate transit to different states on the same trigger set, then
+        #     They can only be covered by barren trigger sets of the candidate.
+        if diff_in_both.is_empty() == False:
+            if not CandidateFatTransitionMap.is_superset(diff_in_both): return None
+
+        # (3) what is in candidate's 'fat' but not in self's 'fat transition' 
+        #     can **only** be covered by wildcards.
+        used_wildcard_set = NumberSet()
+        if diff_not_in_A_in_B.is_empty() == False:
+            if not self.__wildcard_set.is_superset(diff_in_both): return None
+            # Take the reference to 'diff_not_in_A_in_B' to avoid object creation
+            used_wildcard_set = diff_not_in_A_in_B
+            used_wildcard_set.intersect_with(self.__wildcard_set)
+
+        return used_wildcard_set
+
+def categorize(SM, MaxComparisonN):
+
+    # A state can only by absorbed by one other state.
+    # => book keeping about each state that has been absorbed already.
+    done_set = set([])
+
+    tree_db  = {}
+    # Here, the loop determines the sequence of processing, therefore 
+    # determines the **priorization** of where states are absorbed. States that
+    # come first, can absorb a subsequent state, later states **must** transit
+    # to the subsequent state.
+    for state_index, state in SM.items():
+        barren, fat = categorize_transitions(State, MaxComparisonN)
+        for target_index, trigger_set in barren.items():
+            if target_index in done_set: 
+                # Move transition to 'fat transitions'
+                fat[target_index] = trigger_set
+                del barren[target_index]
+            else:
+                done_set.add(target_index)
+
+        if barren.is_empty(): barren = None
+        tree_db[state_index] = TreeBase(state, fat, barren)
+
+    # Barren transitions to states which misfit become part of the fat transitions.
+    info_changed_f = False
+    while 1 + 1 == 2:
+
+        for state_index, info in sorted(tree_db.items(), my_sort):
+
+            transition_change_f = False
+            while 1 + 1 == 2:
+
+                backup = info.wildcard_set()
+
+                for target_index, trigger_set in info.barren_transition_map():
+                    target_info        = tree_db[target_index]
+                    required_wildcards = info.match(target_info)
+                    #
+                    if required_wildcards == None:
+                        # Misfit --> 'fat transition'
+                        info.move_to_fat(target_index)
+                        transition_change_f = True
+                        info_changed_f      = True
+                        break
+
+                    elif required_wildcards.is_empty() == False:
+                        info.plug_wildcards(required_wildcards)
+
+                # While there was a change in the 'fat transitions' we need to check
+                # again if the fat transitions match.
+                if not transition_change_f: break
+                
+                info.set_wildcard_set(backup)
+
+        if not info_changed_f: break
+
+    return tree_db
+
+def categorize_transitions(State, MaxComparisonN):
+    """This function categorizes the transitions of a state into 'barren' and
+       'fat' transitions. That is, barren transitions happen on small character
+       sets that can be compared with less or equal MaxComparisonN. Any
+       transition taking more comparisons than that are 'fat' transitions.
+    """
+    transition_map = State.transitions().get_map()
+
+    barren_transition_map = {}
+    for target_index, trigger_set in transition_map.items():
+        interval_list = trigger_set.get_intervals(PromiseToTreatWellF=True)
+        cmp_required  = 0
+        for interval in interval_list:
+            if interval.end - interval.begin < 2: cmp_required += 1 # Check '==' begin
+            else:                                 cmp_required += 2 # Check '>=' begin and '<' end
+            if cmp_required > MaxComparisonN: break
+        if cmp_required > MaxComparisonN: 
+            fat_transition_map[target_index] = trigger_set
+        else:
+            barren_transition_map[target_index] = trigger_set
+
+    return barren_transition_map, fat_transition_map
+
+
