8ac9c265a337f8f328ca931592eed9db89bed2be
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2016-08-28 14:51:23 +0000

    Towards Release 0.66.1: UTs 153/1489 still fail.
    git-svn-id: https://svn.code.sf.net/p/quex/code/trunk@4488 db9deaaf-66c7-40b0-811a-ad7aae109365

---
+++ TEST/call-make.sh
@@ -0,0 +1,18 @@
+tmpfile=$(mktemp --suffix $(basename $1))
+
+make "$@" ASSERTS_ENABLED_F=YES >& $tmpfile
+
+cat $tmpfile \
+    | awk '   /[Ww][Aa][Rr][Nn][Ii][Nn][Gg]/ \
+           || /[Ee][Rr][Rr][Oo][Rr]/'        \
+    | awk '    ! /out of range/              \
+            && ! /getline/                   \
+            && ! /-Werror/                   \
+            && ! /gcc/                       \
+            && ! /g\+\+/                     \
+            && ! /ASSERTS/                   \
+            && ! /deprecated since quex/     \
+            && ! /QUEX_ERROR_EXIT/           \
+            && ! /QUEX_ERROR_DEPRECATED/'   
+
+rm $tmpfile
