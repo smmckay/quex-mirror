fe84f1ef659095eae7c67c4b71965a0d403130a4
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2016-10-27 06:48:55 +0000

    UT on line numbe pragmas improved
    git-svn-id: https://svn.code.sf.net/p/quex/code/trunk@4555 db9deaaf-66c7-40b0-811a-ad7aae109365

--- quex/TESTS/misc/TEST/line-number-pragma.sh
+++ quex/TESTS/misc/TEST/line-number-pragma.sh
@@ -1,29 +1,56 @@
 #! /usr/bin/env bash
 
+if [ "$2" = "FIRST" ] || [ "$2" = "" ]; then 
+    quex -i line-number-pragma.qx -o LineNumberPragma
+fi
+
+file_list='LineNumberPragma LineNumberPragma-token LineNumberPragma.cpp'
+
 case $1 in
     --hwut-info)
         echo "Testing the implementation of line number pragmas;"
-        echo "HAPPY: :[0-9]+:;"
+        echo "CHOICES: qx, source;"
+        echo "HAPPY:   :[0-9]+:;"
+        exit;
         ;;
 
-    *)
-        quex -i line-number-pragma.qx -o LineNumberPragma
-        cat LineNumberPragma       \
-            LineNumberPragma-token \
-            LineNumberPragma.cpp   >> tmp.txt
+    qx)
         echo __________________________________________________________________
-        echo Pragmas
+        echo Pragmas from within .qx file
         echo
-        echo __________________________________________________________________
-        echo Implementations
-        echo
-        for file in LineNumberPragma.cpp LineNumberPragma LineNumberPragma-token; do
-            echo $file
-            echo
-            grep -sHIne '# *line' $file;
-        done
+        tmp_file=$(mktemp)
+        cat $file_list > $tmp_file
+        grep -sHIne '# *line' $tmp_file | grep -soe '[0-9]\+ \+\"line-number-pragma.qx\"' | sort -nu;
+        rm -f $tmp_file
+        ;;
 
-        echo "remove"
-        #rm -f LineNumberPragma* tmp.txt
+    source)
+        echo __________________________________________________________________
+        echo Pragmas to set-back the file internal line numbers.
+        echo
+        for file in $file_list; do
+            echo $file
+            # * Extract line number pragmas about the file itself!
+            #              OUTPUT: => file:N: #line M "file"
+            # * Delete everything except N and M of each line
+            #              OUTPUT: => N M
+            # * Ensure that:
+            #              N = M - 1
+            # * No output is good output
+            echo "   <no output is good output/print only errors>"
+            grep -sHIne "# *line \+[0-9]\+ \+\"$file\"" $file \
+            | sed -e "s/$file//g" \
+            | sed -e "s/line//g"  \
+            | tr -d "\"#:"        \
+            | awk 'BEGIN { n = 0; } { n += 1; if( $1 != $2 - 1 ) print "$1 != $2 - 1"; } END { print "   count: " n; }'
+        done
+        
         ;;
 esac
+
+echo __________________________________________________________________
+if [[ "$3" = "LAST" ]] || [[ "$3" = "" ]]; then 
+    # echo "Do not forget to 'remove'"
+    rm LineNumberPragma*
+fi
+echo "<terminated>"
