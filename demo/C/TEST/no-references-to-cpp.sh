64cf137bf44de193c44e508a611efe6009e35326
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2016-12-03 20:05:07 +0000

    UTs
    git-svn-id: https://svn.code.sf.net/p/quex/code/trunk@4623 db9deaaf-66c7-40b0-811a-ad7aae109365

--- demo/C/TEST/no-references-to-cpp.sh
+++ demo/C/TEST/no-references-to-cpp.sh
@@ -6,10 +6,14 @@ fi
 
 echo
 echo "-- Check for references to 'Cpp demos' (no output is good output) --"
-grep -sHIne '/Cpp/' *.sh | awk '! /no-references/ && ! /makefile-diff/' | sort
+echo "||||"
+grep -sHIne '/Cpp/' *.sh | awk '! /no-references/ && ! /makefile-diff/'
+echo "||||"
 echo "--"
 echo
 echo "-- Check for references to 'Cpp demos' --"
-grep -sHIne '/C/' *.sh | awk '! /no-references/ && ! /makefile-diff/' | sort
+echo "||||"
+grep -sHIne '/C/' *.sh | awk '! /no-references/ && ! /makefile-diff/'
+echo "||||"
 echo "--"
 echo "<terminated>"
