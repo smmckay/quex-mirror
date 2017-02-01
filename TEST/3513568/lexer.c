d7cd746de989431fb64cdfccc06c3b2590d64354
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2017-02-01 10:53:57 +0000

    Towards 0.67.1; UT 12/1638
    git-svn-id: https://svn.code.sf.net/p/quex/code/trunk@4700 db9deaaf-66c7-40b0-811a-ad7aae109365

--- TEST/3513568/lexer.c
+++ TEST/3513568/lexer.c
@@ -5,7 +5,8 @@ int
 main(int argc, char** argv) 
 {        
     boeck_Lexer   x;
-    boeck_Lexer_from_file_name(&x, "empty.txt", "UCS4");
+    QUEX_NAME(Converter)*  converter = QUEX_NAME(Converter_ICU_new)("UCS4", NULL);
+    boeck_Lexer_from_file_name(&x, "empty.txt", converter);
     boeck_Lexer_destruct(&x);
 
     return 0;
