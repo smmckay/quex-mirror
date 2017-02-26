6fabbca26c9740f963309487045127bdec28b65f
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2017-02-26 22:47:05 +0000

    Extracting ICU/IConv from core engine
    git-svn-id: https://svn.code.sf.net/p/quex/code/trunk@4747 db9deaaf-66c7-40b0-811a-ad7aae109365

--- TEST/bug-2095970.sh
+++ TEST/bug-2095970.sh
@@ -14,23 +14,25 @@ make clean >& /dev/null
 
 
 if [[ $1 == "Normal" ]]; then
-    make EXT_MODE_FILE=with-mode-change-detection.qx 
+    make EXT_MODE_FILE=with-mode-change-detection.qx >& /dev/null
 fi
 if [[ $1 == "NormalNoAsserts" ]]; then
     make EXT_MODE_FILE=with-mode-change-detection.qx \
-         EXT_CFLAGS=-DQUEX_OPTION_ASSERTS_DISABLED     
+         EXT_CFLAGS=-DQUEX_OPTION_ASSERTS_DISABLED     >& /dev/null
 fi
 if [[ $1 == "NoModeDetection" ]]; then
     make EXT_MODE_FILE=without-mode-change-detection.qx \
-         EXT_CFLAGS=-DQUEX_OPTION_AUTOMATIC_ANALYSIS_CONTINUATION_ON_MODE_CHANGE_DISABLED
+         EXT_CFLAGS=-DQUEX_OPTION_AUTOMATIC_ANALYSIS_CONTINUATION_ON_MODE_CHANGE_DISABLED >& /dev/null
 fi
 if [[ $1 == "NoModeDetectionNoAsserts" ]]; then
     make EXT_MODE_FILE=without-mode-change-detection.qx \
-         EXT_CFLAGS='-DQUEX_OPTION_AUTOMATIC_ANALYSIS_CONTINUATION_ON_MODE_CHANGE_DISABLED -DQUEX_OPTION_ASSERTS_DISABLED'
+         EXT_CFLAGS='-DQUEX_OPTION_AUTOMATIC_ANALYSIS_CONTINUATION_ON_MODE_CHANGE_DISABLED \
+         -DQUEX_OPTION_ASSERTS_DISABLED' >& /dev/null
 fi
 if [[ $1 == "NoModeDetection_ErrorCase" ]]; then
     make EXT_MODE_FILE=with-mode-change-detection.qx \
-         EXT_CFLAGS='-DQUEX_OPTION_AUTOMATIC_ANALYSIS_CONTINUATION_ON_MODE_CHANGE_DISABLED'
+         EXT_CFLAGS='-DQUEX_OPTION_AUTOMATIC_ANALYSIS_CONTINUATION_ON_MODE_CHANGE_DISABLED' \
+         >& /dev/null
 fi
 
 ./lexer >& tmp.txt
