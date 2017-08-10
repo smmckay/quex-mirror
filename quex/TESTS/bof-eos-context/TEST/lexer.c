5e87daf109c6dedaf2306b835635dce73c28526e
fschaef <fschaef@db9deaaf-66c7-40b0-811a-ad7aae109365>
2017-08-10 17:21:39 +0000

    Improved UTs on Contexts of Begin/End of Stream
    git-svn-id: https://svn.code.sf.net/p/quex/code/trunk@4909 db9deaaf-66c7-40b0-811a-ad7aae109365

--- quex/TESTS/bof-eos-context/TEST/lexer.c
+++ quex/TESTS/bof-eos-context/TEST/lexer.c
@@ -4,23 +4,23 @@
 
 int main(int argc, char** argv)
 {
-    quex_Token*      token_p = 0x0;
-    LEXER_TYPE       tlex;
+    quex_Token*  token_p = 0x0;
+    LEXER_TYPE   tlex;
+    const char*  file_name = (const char*)0;
+#   define       BufferSize 1024
+    char         buffer[BufferSize];
 
     hwut_info(TITLE ";\n" \
-              "CHOICES: 1, 2;");
+              "CHOICES: bos-and-eos.txt, bos-eos.txt, bos-x-x-eos.txt, not-bos-eos.txt;");
 
-    hwut_if_choice("1") {
-        QUEX_NAME(from_file_name)(&tlex, "1.txt", /* Converter */NULL);
-    }
-    hwut_if_choice("2") {
-        QUEX_NAME(from_file_name)(&tlex, "2.txt", /* Converter */NULL);
-    }
+    file_name = argv[1];
+
+    QUEX_NAME(from_file_name)(&tlex, file_name, /* Converter */NULL);
 
     do {
         QUEX_NAME(receive)(&tlex, &token_p);
 
-        printf("%s: %s;\n", QUEX_NAME_TOKEN(map_id_to_name)(token_p->id), token_p->text);
+        printf("%s;\n", QUEX_NAME_TOKEN(get_string)(token_p, buffer, BufferSize));
 
     } while(    token_p->id     != QUEX_TKN_TERMINATION
              && tlex.error_code == E_Error_None );
